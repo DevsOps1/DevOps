08da8d4bb3bcf75dc63a18be638ec4ea6076bda8
Stylian LAMIRAY <s.lamiray@iia-laval.fr>
2021-03-03 11:46:02 +0100

    update calculatrice

---
+++ Projet_Calculatrice/Projet_Calculatrice/calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet_Calculatrice
+{
+    public class calc
+    {
+        public calc()
+        {
+            int add(int a, int b)
+            {
+                return a + b;
+            }
+
+            int moins(int a, int b)
+            {
+                return a - b;
+            }
+        }
+    }
+}
