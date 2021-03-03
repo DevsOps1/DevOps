d91ee3696dc16938b8b0006825cb5e9a9db417ff
Stylian LAMIRAY <s.lamiray@iia-laval.fr>
2021-03-03 10:27:43 +0100

    adding solution

---
+++ Projet_Calculatrice/Projet_Calculatrice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet_Calculatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
